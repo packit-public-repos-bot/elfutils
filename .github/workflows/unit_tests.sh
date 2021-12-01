91c507a8cab79042a03fbbdfe8d41e2ca33e37e0
Evgeny Vereshchagin <evvers@ya.ru>
2021-12-01 21:40:55 +0000

    ci: Coverity

--- .github/workflows/unit_tests.sh
+++ .github/workflows/unit_tests.sh
@@ -13,8 +13,63 @@ ADDITIONAL_DEPS=(
     valgrind
     lcov
 )
+COVERITY_SCAN_TOOL_BASE="/tmp/coverity-scan-analysis"
+COVERITY_SCAN_PROJECT_NAME="evverx/elfutils"
 
 set -ex
+
+function coverity_install_script {
+    set +x # This is supposed to hide COVERITY_SCAN_TOKEN
+    local platform=$(uname)
+    local tool_url="https://scan.coverity.com/download/${platform}"
+    local tool_archive="/tmp/cov-analysis-${platform}.tgz"
+
+    echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
+    wget -nv -O $tool_archive $tool_url --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN" || return
+
+    echo -e "\033[33;1mExtracting Coverity Scan Analysis Tool...\033[0m"
+    mkdir -p $COVERITY_SCAN_TOOL_BASE
+    pushd $COVERITY_SCAN_TOOL_BASE
+    tar xzf $tool_archive || return
+    popd
+    set -x
+}
+
+function run_coverity {
+    set +x # This is supposed to hide COVERITY_SCAN_TOKEN
+    local results_dir="cov-int"
+    local tool_dir=$(find $COVERITY_SCAN_TOOL_BASE -type d -name 'cov-analysis*')
+    local results_archive="analysis-results.tgz"
+    local sha=$(git rev-parse --short HEAD)
+    local response status_code
+
+    echo -e "\033[33;1mRunning Coverity Scan Analysis Tool...\033[0m"
+    COVERITY_UNSUPPORTED=1 $tool_dir/bin/cov-build --dir $results_dir sh -c "make -j V=1" || return
+    $tool_dir/bin/cov-import-scm --dir $results_dir --scm git --log $results_dir/scm_log.txt || return
+
+    echo -e "\033[33;1mTarring Coverity Scan Analysis results...\033[0m"
+    tar czf $results_archive $results_dir || return
+
+    echo -e "\033[33;1mUploading Coverity Scan Analysis results...\033[0m"
+    response=$(curl \
+               --silent --write-out "\n%{http_code}\n" \
+               --form project=$COVERITY_SCAN_PROJECT_NAME \
+               --form token=$COVERITY_SCAN_TOKEN \
+               --form email=$COVERITY_SCAN_EMAIL \
+               --form file=@$results_archive \
+               --form version=$sha \
+               --form description="Daily build" \
+               https://scan.coverity.com/builds)
+    printf "\033[33;1mThe response is\033[0m\n%s\n" "$response"
+    status_code=$(echo "$response" | sed -n '$p')
+    if [ "$status_code" != "200" ]; then
+        echo -e "\033[33;1mCoverity Scan upload failed: $(echo "$response" | sed '$d').\033[0m"
+        return 1
+    fi
+
+    echo -e "\n\033[33;1mCoverity Scan Analysis completed successfully.\033[0m"
+    set -x
+}
 
 for phase in "${PHASES[@]}"; do
     case $phase in
@@ -55,6 +110,12 @@ for phase in "${PHASES[@]}"; do
             if [[ "$phase" != "RUN_CLANG" ]]; then
                 make V=1 distcheck
             fi
+            ;;
+        COVERITY)
+            coverity_install_script
+            autoreconf -i -f
+            ./configure --enable-maintainer-mode
+            run_coverity
             ;;
         *)
             echo >&2 "Unknown phase '$phase'"
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 [![Coverage Status](https://coveralls.io/repos/github/evverx/elfutils/badge.svg?branch=main)](https://coveralls.io/github/evverx/elfutils?branch=main)
 [![Unit tests](https://github.com/evverx/elfutils/actions/workflows/unit_tests.yml/badge.svg?branch=main)](https://github.com/evverx/elfutils/actions/workflows/unit_tests.yml)
 [![OSS-Fuzz Status](https://oss-fuzz-build-logs.storage.googleapis.com/badges/elfutils.svg)](https://oss-fuzz-build-logs.storage.googleapis.com/index.html#elfutils)
+[![Coverity Scan Status](https://scan.coverity.com/projects/24183/badge.svg)](https://scan.coverity.com/projects/evverx-elfutils)
 
 [README](./README)
