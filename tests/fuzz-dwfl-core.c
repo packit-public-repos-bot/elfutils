17f5630e6b6e39f9e57baabb2eebc0e34b908e4c
Evgeny Vereshchagin <evvers@ya.ru>
2022-01-12 03:37:36 +0000

    add a "modules" callback

--- tests/fuzz-dwfl-core.c
+++ tests/fuzz-dwfl-core.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <config.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include ELFUTILS_HEADER(dwfl)
 #include "fuzz.h"
@@ -18,6 +19,48 @@ static const Dwfl_Callbacks core_callbacks =
     .find_elf = dwfl_build_id_find_elf,
     .find_debuginfo = dwfl_standard_find_debuginfo,
   };
+
+static int
+module_callback (Dwfl_Module *mod, void **userdata __attribute__((unused)),
+		 const char *name, Dwarf_Addr start,
+		 void *arg __attribute__((unused)))
+{
+  /* Forces resolving of main elf and debug files. */
+  Dwarf_Addr bias;
+  Elf *elf = dwfl_module_getelf (mod, &bias);
+  Dwarf *dwarf = dwfl_module_getdwarf (mod, &bias);
+
+  Dwarf_Addr end;
+  const char *mainfile;
+  const char *debugfile;
+  const char *modname = dwfl_module_info (mod, NULL, NULL, &end, NULL,
+                                          NULL, &mainfile, &debugfile);
+  if (modname == NULL || strcmp (modname, name) != 0)
+    {
+      end = start + 1;
+      mainfile = NULL;
+      debugfile = NULL;
+    }
+
+  const unsigned char *id;
+  GElf_Addr id_vaddr;
+  int id_len = dwfl_module_build_id (mod, &id, &id_vaddr);
+  if (id_len > 0)
+    {
+      printf ("  [");
+      do
+	printf ("%02" PRIx8, *id++);
+      while (--id_len > 0);
+      printf ("]\n");
+    }
+
+  if (elf != NULL)
+    printf ("  %s\n", mainfile != NULL ? mainfile : "-");
+  if (dwarf != NULL)
+    printf ("  %s\n", debugfile != NULL ? debugfile : "-");
+
+  return DWARF_CB_OK;
+}
 
 int
 LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
@@ -47,6 +90,8 @@ LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
   if (dwfl_core_file_report (dwfl, core, NULL) < 0)
     goto cleanup;
   if (dwfl_report_end (dwfl, NULL, NULL) != 0)
+    goto cleanup;
+  if (dwfl_getmodules (dwfl, module_callback, NULL, 0) != 0)
     goto cleanup;
 
 cleanup:
